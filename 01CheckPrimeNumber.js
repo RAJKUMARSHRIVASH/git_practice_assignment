0889a506a9ac285ce75a84de12d3f2e36f44a976
RAJ KUMAR SEN <rajkumarshrivash374@gmail.com>
2022-09-13 20:28:04 +0530

    comments added and logic defined

--- 01CheckPrimeNumber.js
+++ 01CheckPrimeNumber.js
@@ -1,10 +1,16 @@
 
-// Problem is to Check whether the number given is Prime number or not
+// Problem is to Check whether the number given is Prime number or not.
 
 let num = 13;
 let factor = 0;
 
 for(let i = 0; i<=num; i++){
+
+
+    // here or logic is we know that to be a prime number the number should be divisible by 1 or itself only
+    // in other words we can also say that it will be having only 2 factors i.e. factor 1 an itself
+    // as it wont be divisible by anyone so using property as a logic.
+
     if(num%i == 0){
         factor++;
     }
